18520fe7c0ee3d0abae6374ecacb6867a8a6b87b
Michael Clay <sayinplot@gmail.com>
2022-08-25 23:35:50 +0300

    for EC2

--- flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
+++ flask-04-handling-forms-POST-GET-Methods/Flask_GET_POST_Methods/app.py
@@ -37,4 +37,5 @@ def calculate():
 
 # Add a statement to run the Flask application which can be debugged.
 if __name__== "__main__":
-    app.run(debug=True, port=3000)
+# app.run(debug=True, port=2000)
+      app.run(host='0.0.0.0', port=80)
